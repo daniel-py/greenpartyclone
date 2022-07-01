9618c2190095abe477e3579c3163c2dad0dd7338
Daniel <doluremi19@gmail.com>
2022-07-01 17:39:36 +0100

    updated styles

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ function docReady(fn) {
 
 docReady(function() {
     // DOM is loaded and ready for manipulation here
-    let menu = document.querySelector(".menu");
-    let menu_dd = document.querySelector("ul.appear");
+    let menu = document.querySelector("ul.appear > i");
+    let menu_dd = document.querySelector("ul.appear > div");
     let check = 0;
 
     menu.addEventListener('click', function(e){
-        if(check===0){
+        if(check==0){
+            console.log("clicked on");
             menu_dd.style.display = "block";
+            menu.style.color = "green";
+            check = 1;
+        } else {
+            console.log("clicked off");
+            menu_dd.style.display = "none";
+            menu.style.color = "#52df00";
+            check = 0;
         }
     })
 });
